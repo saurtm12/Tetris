799a20d159535ffb7000634bd69deaa0d67f3789
Duc Hong <duc.hong@tuni.fi>
2020-01-12 20:23:02 +0200

    a

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -7,7 +7,7 @@ int main()
     int temp;
     cout << "Enter a temperature: ";
     cin >> temp;
-    cout << temp <<" degrees Celsius is "<< temp*9/5+32 <<" degrees Fahrenheit"<< endl;
-    cout << temp <<" degrees Fahrenheit is "<< (temp-32)/1.8  <<" degrees Celsius";
+    cout << temp <<" degrees Celsius is "<< (float)temp*9/5+32 <<" degrees Fahrenheit"<< endl;
+    cout << temp <<" degrees Fahrenheit is "<< (float)(temp-32)/1.8  <<" degrees Celsius";
     return 0;
 }
