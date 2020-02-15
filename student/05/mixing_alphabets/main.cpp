ef25d87f2c75ebc200ad2fc88e1bbf66c4832477
Duc Hong <duc.hong@tuni.fi>
2020-02-15 02:13:38 +0200

    fixed

--- student/05/mixing_alphabets/main.cpp
+++ student/05/mixing_alphabets/main.cpp
@@ -10,10 +10,9 @@ int main()
     // This is a random number generator that should be given as parameter to the
     // function of the algorithm library to shuffle letters
     std::minstd_rand generator;
-    generator.seed(time(NULL));
+
     std::cout << "Enter some text. Quit by entering the word \"END\"." << std::endl;
     std::string word;
-
     while (std::cin >> word)
     {
         if (word == "END")
