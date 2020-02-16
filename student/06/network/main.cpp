aa9d3295ffd856c73e4a17b6ce51ea663dc5a2fc
Duc Hong <duc.hong@tuni.fi>
2020-02-17 00:11:20 +0200

    first submission network

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <map>
+#include <queue>
+#include <set>
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
 
@@ -26,12 +28,85 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     }
     return result;
 }
+int insert_relation(std::string id1, std::string id2,
+                    std::map < std::string, std::vector <std::string> > & tree)
+{
+    std::map < std::string, std::vector <std::string> > ::iterator iter;
+    iter = tree.find(id1);
+    if (tree.find(id2)== tree.end())
+    {
+        tree.insert({id2,{}});
+    }
+    tree.insert({id2,{}});
+    if (iter == tree.end())
+    {
+        tree.insert({id1,{id2}});
 
+    }
+    else {
+        iter->second.push_back(id2);
+    }
+    return 0;
+
+}
+int P_command(std::string id, std::map < std::string, std::vector <std::string> > tree, std::set <std::string> &checked,int depth =0)
+{
+
+    checked.insert(id);
+    for (int k =0; k<depth;k++)
+    {
+        std::cout<<"..";
+    }
+    std::cout<< id << std::endl;
+    for (auto id_ : tree.at(id))
+    {
+        if (checked.find(id_) == checked.end())
+            P_command(id_, tree, checked,depth+1);
+    }
+    return 0;
+}
+
+int C_command(std::string id, std::map < std::string, std::vector <std::string> > tree, std::set <std::string> &checked)
+{
+    checked.insert(id);
+    if (tree.at(id).size() == 0)
+        return 1;
+    int S =0;
+    for (auto id_ : tree.at(id))
+    {
+
+        if (checked.find(id_) == checked.end())
+            S+= C_command(id_, tree, checked);
+    }
+    return S+1;
+}
+
+
+int D_command(std::string id, std::map < std::string, std::vector <std::string> > tree, std::set <std::string> &checked)
+{
+    checked.insert(id);
+    if (tree.at(id).size() == 0)
+        return 1;
+    int S =0;
+    for (auto id_ : tree.at(id))
+    {
+
+        if (checked.find(id_) == checked.end())
+        {
+                int K= D_command(id_, tree, checked);
+                if (K > S )
+                    S=K;
+        }
+    }
+    return S+1;
+}
 
 
 int main()
 {
     // TODO: Implement the datastructure here
+    std::map < std::string, std::vector <std::string> > tree;
+
 
 
     while(true){
@@ -51,23 +126,26 @@ int main()
             std::string id2 = parts.at(2);
 
             // TODO: Implement the command here!
-
+            insert_relation(id1,id2,tree);
         } else if(command == "P" or command == "p"){
             if(parts.size() != 2){
                 std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
                 continue;
+
             }
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
-
+            std::set <std::string> checked;
+            P_command(id,tree,checked);
         } else if(command == "C" or command == "c"){
             if(parts.size() != 2){
                 std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
                 continue;
             }
             std::string id = parts.at(1);
-
+            std::set <std::string> checked;
+            std::cout<< C_command(id,tree,checked) - 1<<std::endl;
             // TODO: Implement the command here!
 
         } else if(command == "D" or command == "d"){
@@ -78,6 +156,8 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            std::set <std::string> checked;
+            std::cout<< D_command(id,tree,checked) <<std::endl;
 
         } else if(command == "Q" or command == "q"){
            return EXIT_SUCCESS;
