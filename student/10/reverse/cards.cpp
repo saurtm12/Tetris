610ff824216589c22df56737b5ed7589331d3a1a
Duc Hong Nghia <duc.hong@tuni.fi>
2020-03-11 03:00:42 +0200

    second submit reverse

--- student/10/reverse/cards.cpp
+++ student/10/reverse/cards.cpp
@@ -33,30 +33,19 @@ bool Cards::remove(int& id)
     }
 }
 
-void Cards::add_bottom(int x)
-{
-    if (top_.get() == nullptr)
-    {
-        add(x);
-    }
-    else
-    {
-        int id;
-        remove(id);
-        add_bottom(x);
-        add(id);
-    }
-}
-
 void Cards::reverse()
 {
-    if (top_.get() != nullptr)
-    {
-        int id;
-        remove(id);
-        reverse();
-        add_bottom(id);
-    }
+     std::shared_ptr<Card_data> cur = top_;
+     std::shared_ptr<Card_data> pre = nullptr;
+     std::shared_ptr<Card_data> next = nullptr;
+     while (cur.get() != nullptr)
+     {
+         next= cur->next;
+         cur->next = pre;
+         pre = cur;
+         cur = next;
+     }
+     top_ = pre;
 }
 // Do not write the stubs of the methods remove and reverse by yourself here,
 // but open the file reverse.hh and click the declaration of the method
