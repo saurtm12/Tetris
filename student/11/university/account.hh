ccaef03be35564bb993ce1864c2f6789902ac240
Nghia Duc Hong <duc.hong@tuni.fi>
2020-04-14 14:05:25 +0700

    complete declare functions in account.hh

--- student/11/university/account.hh
+++ student/11/university/account.hh
@@ -43,12 +43,38 @@ public:
      */
     std::string get_email();
 
+    /**
+     * @brief print all the courses that have been completed
+     */
+    void print_complete_courses() const;
+
+    /**
+     * @brief print study state (current and completed) of an account
+     */
+    void print_study_state() const;
+
+    /**
+     * @brief add instance to account
+     * @param instance as "the instance need to be added"
+     */
+    void add_instance(Instance* instance);
+
+    /**
+     * @brief use to finish an instance
+     * @param instance as "the instance need to be completed"
+     */
+    void complete_instance(Instance* instance);
+
 private:
     std::string full_name_;
     std::string last_name_;
     std::string first_name_;
     std::string email_;
     const int account_number_;
+
+    //store credist have been earned.
+    int credit_;
+
     std::vector<Instance*> current_;
     std::vector<Course*> completed_;
 };
