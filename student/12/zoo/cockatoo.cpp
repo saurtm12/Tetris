bb3d6e168c1965b9be18fa52fe63bff8b6d9836b
Nghia Duc Hong <duc.hong@tuni.fi>
2020-04-15 17:03:31 +0700

    second submission

--- student/12/zoo/cockatoo.cpp
+++ student/12/zoo/cockatoo.cpp
@@ -4,7 +4,6 @@ Cockatoo::Cockatoo()
 {
 
 }
-
 void Cockatoo::sing(std::ostream &output) const
 {
  output << "Kee-ow kee-ow!" << std::endl;
--- student/12/zoo/cockatoo.hh
+++ student/12/zoo/cockatoo.hh
@@ -6,6 +6,7 @@ class Cockatoo: public Bird
 {
 public:
     Cockatoo();
+    
     void sing(std::ostream& output) const;
 };
 
