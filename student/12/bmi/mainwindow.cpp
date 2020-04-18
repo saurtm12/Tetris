2cba46fec9072530b0104130b49799ca503dfc57
Duc Hong <duc.hong@tuni.fi>
2020-04-19 01:29:51 +0700

    Revert "push to revert"
    This reverts commit 2bfc8437e4e1990dd2e84ef4983886493668500a.

--- student/12/bmi/mainwindow.cpp
+++ student/12/bmi/mainwindow.cpp
@@ -6,17 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->countButton, &QPushButton::clicked, this, &MainWindow::count)
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-MainWindow::count()
-{
-    auto height = ui->heightLineEdit->text().toStdString();
-    auto weight = ui->weightLineEdit->text().toStdString();
-
 }
 
--- student/12/bmi/mainwindow.hh.autosave
+++ student/12/bmi/mainwindow.hh.autosave
@@ -17,5 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+private slots:
+    void count();
 };
 #endif // MAINWINDOW_HH
