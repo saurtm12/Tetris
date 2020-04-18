2bfc8437e4e1990dd2e84ef4983886493668500a
Duc Hong <duc.hong@tuni.fi>
2020-04-19 01:29:19 +0700

    push to revert

--- student/12/bmi/mainwindow.cpp
+++ student/12/bmi/mainwindow.cpp
@@ -6,10 +6,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->countButton, &QPushButton::clicked, this, &MainWindow::count)
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+MainWindow::count()
+{
+    auto height = ui->heightLineEdit->text().toStdString();
+    auto weight = ui->weightLineEdit->text().toStdString();
+
 }
 
