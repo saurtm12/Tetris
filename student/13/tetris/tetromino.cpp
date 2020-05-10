8c3b4ed0012c5e7ca7fa8fda0f7c49724d5ad28f
Duc Hong <duc.hong@tuni.fi>
2020-05-10 20:00:34 +0700

    implement functions of class tetromino

--- student/13/tetris/tetromino.cpp
+++ student/13/tetris/tetromino.cpp
@@ -1,6 +1,33 @@
 #include "tetromino.hh"
 
+Tetromino::Tetromino(const int& tetromino_kind_, const int& color_, Positions initial_pos_):
+    tetromino_kind(tetromino_kind_),
+    initial_pos(initial_pos_),
+    color(color_)
+{
+
+}
+
 Tetromino::Tetromino()
 {
 
+}
+
+Tetromino::~Tetromino()
+{
+
+}
+int Tetromino::get_kind() const
+{
+    return tetromino_kind;
+}
+
+int Tetromino::get_color() const
+{
+    return color;
+}
+
+Positions Tetromino::get_initial_pos() const
+{
+    return initial_pos;
 }
