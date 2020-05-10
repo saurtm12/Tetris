de0566e28377324ec4ece5b76fcd1df795400e09
Duc Hong <duc.hong@tuni.fi>
2020-05-10 19:59:54 +0700

    declare class Tetromino

--- student/13/tetris/tetromino.hh
+++ student/13/tetris/tetromino.hh
@@ -1,11 +1,36 @@
 #ifndef TETROMINO_HH
 #define TETROMINO_HH
 
+#include <vector>
+
+using Positions = std::vector<std::pair<int,int>>;
 
 class Tetromino
 {
 public:
+    Tetromino(const int& tetromino_kind_, const int& color_, Positions initial_pos_);
     Tetromino();
+    ~Tetromino();
+    //can not disable copy constructor and assign operator because I am using it.
+
+    /**
+     * @brief return the kind of tetromino.
+     */
+    int get_kind() const;
+
+    /**
+     * @brief return the color of tetromino.
+     */
+    int get_color() const;
+
+    /**
+     * @brief return the initial position of tetromino.
+     */
+    Positions get_initial_pos() const;
+private:
+    int tetromino_kind;
+    Positions initial_pos;
+    int color;
 };
 
 #endif // TETROMINO_HH
